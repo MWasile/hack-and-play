dcde50365fbbf6ce77408573ccfed16ce4cacf82
MWasile <maaciej.wasilewski@gmail.com>
2025-10-25 04:31:19 +0200

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from src.routes.district import router as districts_router
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(districts_router, prefix="/api")
 
